aea6b970c0c94e9fb462f88eda994c1f14f16cb1
Muteen Kundangar <kundangarmuteen@gmail.com>
2025-10-16 13:43:05 +0530

    Added created_at filter

--- services/dashboard/courses/filters.py
+++ services/dashboard/courses/filters.py
@@ -5,6 +5,7 @@ from .models import Course
 class CourseFilters(django_filters.FilterSet):
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
     status = django_filters.CharFilter(field_name="access_status", lookup_expr="iexact")
+    created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
 
     class Meta:
         model = Course
