ea725c39150e309f5b5c67fb2b572a4ba3c96800
Muteen Kundangar <kundangarmuteen@gmail.com>
2025-11-06 11:35:28 +0530

    Added API Keys model

--- services/dashboard/apikeys/models.py
+++ services/dashboard/apikeys/models.py
@@ -1,3 +1,22 @@
+from core.models import TimeStampedModel, SoftDeleteMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from instructors.models import Instructor
 
-# Create your models here.
+
+
+class ApiKeyTypes(models.TextChoices):
+    PUBLIC = 'public', _('Public')
+    PRIVATE = 'private', _('Private')
+
+
+class ApiKeys(TimeStampedModel, SoftDeleteMixin):
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name="apikeys")
+    key_name = models.CharField(max_length=255)
+    key_hash = models.CharField(max_length=128)
+    key_type = models.CharField(max_length=50, choices=ApiKeyTypes.choices, default=ApiKeyTypes.PUBLIC)
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed=True
+        db_table="instructors_apikeys"
