eda094f81e1c89f345240d8a7d125ae434eb9d5b
Muteen Kundangar <kundangarmuteen@gmail.com>
2025-12-05 11:03:34 +0530

    Migrated students table

--- config/urls.py
+++ config/urls.py
@@ -34,12 +34,20 @@ schema_view = get_schema_view(
 )
 
 BASE_PATH = 'api/v1/dashboard'
+HEADLESS_PATH = 'api/v1/public'
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Dashboard Routes
     path(f"{BASE_PATH}/internals/", include('core.urls')),
     path(f"{BASE_PATH}/", include('instructors.urls')),
     path(f"{BASE_PATH}/apikeys/", include('apikeys.urls')),
     path(f"{BASE_PATH}/courses/", include('courses.urls')),
+
+    # Headless Routes
+
+    # Swagger + Docs
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
