a02bbd3b79e892af00f6a7aa98e6b2adb24a5265
Muteen Kundangar <kundangarmuteen@gmail.com>
2025-12-06 02:45:41 +0530

    Added NotFoundError Class

--- apps/headless/common/utilities/Errors.py
+++ apps/headless/common/utilities/Errors.py
@@ -6,3 +6,9 @@ class ValidationError(HeadlessException):
     status_code = 400
     default_detail = "Invalid input !"
     error_code = ERR_CODES.VALIDATION_ERR
+
+
+class NotFoundError(HeadlessException):
+    status_code = 404
+    default_detail = "Not Found !"
+    error_code = ERR_CODES.NOT_FOUND_ERR
--- apps/headless/instructor/selectors.py
+++ apps/headless/instructor/selectors.py
@@ -1,11 +1,10 @@
 from apps.dashboard.instructors.models import Instructor, InstructorProfile
 
 
-
 class InstructorProfileSelector:
     @staticmethod
     def get_profile(instructor: Instructor):
         return (
             InstructorProfile.objects
-            .get(instructor_id=1000)
+            .get(instructor=instructor)
         )
