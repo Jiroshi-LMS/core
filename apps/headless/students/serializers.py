50fd08626b8aba0566a6ebc8e90536599879587b
Muteen Kundangar <kundangarmuteen@gmail.com>
2025-12-31 16:14:55 +0530

    Updated current_password constraint on headless student update serializer

--- apps/headless/students/serializers.py
+++ apps/headless/students/serializers.py
@@ -33,4 +33,4 @@ class StudentLoginRequestSerializer(serializers.Serializer):
 class StudentDetailsUpdateSerializer(serializers.Serializer):
     identifier = serializers.CharField(required=False, max_length=255)
     password = serializers.CharField(required=False, max_length=72, min_length=8)
-    current_password = serializers.CharField(required=True)
+    current_password = serializers.CharField(required=True, max_length=72, min_length=8)
