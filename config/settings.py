50b231e5be9ac29cf7ecb10642a8e44c43569f6d
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-01-01 19:05:00 +0530

    Fixed Structlog setting

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ structlog.configure(
     processors=[
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
-        structlog.processors.add_timestamp(fmt="iso"),
+        structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.JSONRenderer(),
