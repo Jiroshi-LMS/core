36f0b78ae340e25530b046a1c536675583c40e45
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-01-02 13:03:51 +0530

    Updated allowed hosts in settings

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DJANGO_DEBUG', 'True'))
 
-ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS', '*')]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
