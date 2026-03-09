07da5f5fe8fc8d29cf899233b6ef2d21bbe8db00
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-03-09 11:21:20 +0530

    Fix: Forced an unimplemented status over unauthorised API routes for instructor on dashboard

--- apps/dashboard/instructors/views.py
+++ apps/dashboard/instructors/views.py
@@ -85,6 +85,10 @@ class InstructorViewSet(viewsets.ModelViewSet):
     @handle_exceptions
     def partial_update(self, request, *args, **kwargs):
         return Res(code=404, msg="Endpoint not implemented").json()
+    
+    @handle_exceptions
+    def destroy(self, request, *args, **kwargs):
+        return Res(code=404, msg="Endpoint not implemented").json()
 
     @handle_exceptions
     @action(detail=False, methods=['POST'], url_path='login')
